8b36988a0c8067de0d6c7719b0f60731fb8f26d1
Aliyah Fajriyani <fajriyanialiyah@gmail.com>
2024-03-05 11:14:58 +0800

    Proyek Akhir

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,17 +6,23 @@ from babel.numbers import format_currency
 
 sns.set(style='dark')
 
+st.header('Dicoding Collection Dashboard :sparkles:')
+st.subheader('Pengaruh Suhu terhadap Peminjaman Sepeda (6 Bulan Terakhir)')
+
 day_hour_df = pd.read_csv("all_data.csv")
 
 six_months_ago = day_hour_df['dteday'].max() - pd.DateOffset(months=6)
 recent_data = day_hour_df[day_hour_df['dteday'] >= six_months_ago]
 
-plt.figure(figsize=(12, 6))
-plt.scatter(recent_data['temp_day'], recent_data['cnt_day'])
-plt.title('Pengaruh Suhu terhadap Peminjaman Sepeda (6 Bulan Terakhir)')
-plt.xlabel('Suhu')
-plt.ylabel('Jumlah Peminjaman')
-plt.show()
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    recent_data["temp_day"],
+    recent_data["cnt_day"],
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
 
 one_year_ago = day_hour_df['dteday'].max() - pd.DateOffset(years=1)
 last_year_data = day_hour_df[day_hour_df['dteday'] >= one_year_ago]
@@ -32,18 +38,19 @@ labels = ['Hari Kerja', 'Hari Libur']
 sizes = [weekday_counts[1], weekday_counts[0]]
 colors = ['lightblue', 'lightgreen']
 
-plt.figure(figsize=(8, 8))
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, startangle=90)
-plt.title('Persentase Peningkatan Peminjaman Sepeda\n(Hari Kerja vs. Hari Libur)')
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, startangle=90)
+ax.set_title('Persentase Peningkatan Peminjaman Sepeda\n(Hari Kerja vs. Hari Libur)')
 plt.show()
 
 busy_hours_data = last_year_data[(last_year_data['hr'] >= 7) & (last_year_data['hr'] <= 19) & (last_year_data['workingday_day'] == 1)]
 
-plt.figure(figsize=(12, 6))
-plt.scatter(busy_hours_data['windspeed_hour'], busy_hours_data['cnt_hour'])
-plt.title('Hubungan Kecepatan Angin terhadap Peminjaman Sepeda (Jam Sibuk, Hari Kerja)')
-plt.xlabel('Kecepatan Angin')
-plt.ylabel('Jumlah Peminjaman')
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.scatter(busy_hours_data['windspeed_hour'], busy_hours_data['cnt_hour'])
+ax.set_title('Hubungan Kecepatan Angin terhadap Peminjaman Sepeda (Jam Sibuk, Hari Kerja)')
+ax.set_xlabel('Kecepatan Angin')
+ax.set_ylabel('Jumlah Peminjaman')
+
 plt.show()
 
 # Hitung jumlah peminjaman pada hari kerja untuk setiap musim
@@ -56,10 +63,11 @@ total_workday_counts = season_workday_counts.sum()
 percentage_by_season = (season_workday_counts / total_workday_counts) * 100
 
 # Visualisasikan dengan diagram batang
-plt.figure(figsize=(10, 6))
-percentage_by_season.plot(kind='bar', color=['lightblue', 'lightgreen', 'lightcoral', 'lightyellow'])
-plt.title('Persentase Peminjaman Sepeda di Berbagai Musim (Hari Kerja)')
-plt.xlabel('Musim')
-plt.ylabel('Persentase Peminjaman')
-plt.xticks(rotation=0)
+fig, ax = plt.subplots(figsize=(10, 6))
+percentage_by_season.plot(kind='bar', color=['lightblue', 'lightgreen', 'lightcoral', 'lightyellow'], ax=ax)
+ax.set_title('Persentase Peminjaman Sepeda di Berbagai Musim (Hari Kerja)')
+ax.set_xlabel('Musim')
+ax.set_ylabel('Persentase Peminjaman')
+ax.set_xticklabels(percentage_by_season.index, rotation=0)
+
 plt.show()
